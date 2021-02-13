65806404e9d9eae305f0507f6e8c2b64ea4de112
karnurmax <karnurmax@mail.ru>
2021-02-14 04:18:56 +0500

    chat example with socket.io

---
+++ server.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const http = require('http');
+const express = require('express');
+const sockjs = require('sockjs');
+
+const sockjs_opts = {
+    prefix: '/echo/info'
+};
+
+const sockjs_echo = sockjs.createServer(sockjs_opts);
+sockjs_echo.on('connection', (conn) => {
+    conn.on('data', (msg) => conn.write(msg));
+});
+
+const app = express();
+app.get('/', (req, res) => res.sendFile(__dirname + '/index.html'));
+
+const server = http.createServer(app);
+// sockjs_echo.attach(server);
+const methods = Object.getOwnPropertyNames(sockjs_echo);
+console.log(methods);
+
+server.listen(9999, '0.0.0.0', () => {
+    console.log(' [*] Listening on 0.0.0.0:9999');
+});
